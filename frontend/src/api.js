12e55bd3ca661ed70d447079a608f70ef441119c
PrajaktaPokhriyal <147468926+PrajaktaPokhriyal@users.noreply.github.com>
2025-11-27 19:02:25 +0530

    Add API module with axios configuration

---
+++ frontend/src/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api'
+});
+
+export function setAuth(token) {
+  API.defaults.headers.common['Authorization'] = token ? `Bearer ${token}` : '';
+}
+
+export default API;
